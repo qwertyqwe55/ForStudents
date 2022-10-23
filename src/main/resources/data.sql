bc77d8ed2a3a8ec250abb359abbb1d56a86a14d4
Gleb Klyauzov <58190010+braindeadboooooy@users.noreply.github.com>
2022-10-23 18:06:40 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,28 @@
 INSERT INTO users VALUES
-(1,'Gleb','Gondon','123','123',TRUE),
-(2,'Petya','Pukin','132','132',FALSE);
+(1,'Gusev','Debil', '19-ПМ-1','gusevDeb','123456','ADMIN'),
+(2,'Gleb','Klyauzov', '19-ПМ-1','klyauzAdm','654321','ADMIN'),
+(3,'Oslan','Shamshetdinov', '19-ПМ-1','shamshAdm','321654', 'ADMIN'),
+(4,'Darya','Kotova', '19-ПМ-1','dKot','213456','STUDENT'),
+(5,'Irina','Dogadina', '19-ПМ-1','irDog','312456','STUDENT'),
+(6,'Maxim','Bushuev', '19-ПМ-1','mxBush','412356','STUDENT'),
+(7,'Ivan','Yakovlev', '19-ПМ-1','ivanGod','512346','STUDENT'),
+(8,'Timofei','Paholkov', '19-ПМ-1','PahTim','612345','STUDENT'),
+(9,'Julia','Butusova', '19-ПМ-1','julyBut','651234','STUDENT'),
+(10,'Kirill','Igoshev', '19-ПМ-1','kirogI','641235','STUDENT'),
+(11,'Alyona','Golovchun', '19-ПМ-1','alGolov4','631245','STUDENT'),
+(12,'LOne','Dubkov', '19-ПМ-2','l0ne','661344','STUDENT'),
+(13,'Vlas','Salv', '19-ПМ-2','vlasAss','663214','STUDENT'),
+(14,'Arina','Motorina', '19-ПМ-2','MotorBOat','666124','STUDENT'),
+(15,'Evgenii','Denisov', '19-ПМ-2','DenEvg','655321','STUDENT'),
+(16,'Ekaterina','Rosibaeva', '19-ПМ-2','RosEka','644123','STUDENT'),
+(17,'Marusya','Narus', '19-ПМ-2','NarusMarus','633543','STUDENT'),
+(18,'Anna','Kozlovsk', '19-ПМ-2','KozlAnn','622312','STUDENT'),
+(19,'Denis','Arapov', '19-ПМ-2','DenisNaHyuPovis','111456','STUDENT'),
+(20,'Tanya','Shep', '19-ПМ-2','ShepTan','222345','STUDENT'),
+(21,'Irina','Boriskova', '19-ПМ-2','BorIrin','531263','STUDENT'),
+(22,'Dmitriy','Smirnov', '19-ПМ-2','SmDm','346616','STUDENT'),
+(23,'Zhenya','Letn', '19-ПМ-2','LetnZh','122365','STUDENT'),
+(24,'Andrey','Bagaev', '0','andrGay','111111','TEACHER'),;
 
 INSERT INTO users1 VALUES
 (1,'Gleb','Gondon','123','123',TRUE),
